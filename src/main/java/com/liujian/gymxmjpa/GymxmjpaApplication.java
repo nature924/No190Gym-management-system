28d5166cdad01e9ed0a376a5fc174550225f56a9
nature924 <1173016385@qq.com>
2024-10-26 21:56:44 +0800

    基于Spring Boot健身房管理系统

---
+++ src/main/java/com/liujian/gymxmjpa/GymxmjpaApplication.java
@@ -0,0 +1,15 @@
+package com.liujian.gymxmjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 悦已健身房管理系统
+ */
+@SpringBootApplication
+public class GymxmjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymxmjpaApplication.class, args);
+    }
+}
